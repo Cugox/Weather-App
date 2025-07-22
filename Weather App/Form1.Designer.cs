b24109cb1bd9b5c456577133b845575faf08d126
Cugox <mgwendi101@gmail.com>
2025-07-22 08:36:28 +0100

    Add files via upload

---
+++ Weather App/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Weather_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.citytxt = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.labCondition = new System.Windows.Forms.Label();
+            this.labDetails = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labSunrise = new System.Windows.Forms.Label();
+            this.labSunset = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labPressure = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labWind = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.picIcon = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labtemp = new System.Windows.Forms.Label();
+            this.humidlbl = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(184, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City";
+            // 
+            // citytxt
+            // 
+            this.citytxt.Location = new System.Drawing.Point(243, 97);
+            this.citytxt.Name = "citytxt";
+            this.citytxt.Size = new System.Drawing.Size(213, 20);
+            this.citytxt.TabIndex = 1;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.BackColor = System.Drawing.Color.Transparent;
+            this.searchBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.searchBtn.ForeColor = System.Drawing.Color.Black;
+            this.searchBtn.Location = new System.Drawing.Point(516, 96);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 30);
+            this.searchBtn.TabIndex = 2;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = false;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // labCondition
+            // 
+            this.labCondition.AutoSize = true;
+            this.labCondition.BackColor = System.Drawing.Color.Transparent;
+            this.labCondition.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labCondition.ForeColor = System.Drawing.Color.Black;
+            this.labCondition.Location = new System.Drawing.Point(219, 157);
+            this.labCondition.Name = "labCondition";
+            this.labCondition.Size = new System.Drawing.Size(108, 19);
+            this.labCondition.TabIndex = 3;
+            this.labCondition.Text = "Condition";
+            // 
+            // labDetails
+            // 
+            this.labDetails.AutoSize = true;
+            this.labDetails.BackColor = System.Drawing.Color.Transparent;
+            this.labDetails.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labDetails.ForeColor = System.Drawing.Color.Black;
+            this.labDetails.Location = new System.Drawing.Point(198, 212);
+            this.labDetails.Name = "labDetails";
+            this.labDetails.Size = new System.Drawing.Size(86, 19);
+            this.labDetails.TabIndex = 4;
+            this.labDetails.Text = "Details";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(198, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Sunrise:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // labSunrise
+            // 
+            this.labSunrise.AutoSize = true;
+            this.labSunrise.BackColor = System.Drawing.Color.Transparent;
+            this.labSunrise.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labSunrise.ForeColor = System.Drawing.Color.Black;
+            this.labSunrise.Location = new System.Drawing.Point(301, 259);
+            this.labSunrise.Name = "labSunrise";
+            this.labSunrise.Size = new System.Drawing.Size(42, 19);
+            this.labSunrise.TabIndex = 6;
+            this.labSunrise.Text = "N/A";
+            // 
+            // labSunset
+            // 
+            this.labSunset.AutoSize = true;
+            this.labSunset.BackColor = System.Drawing.Color.Transparent;
+            this.labSunset.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labSunset.ForeColor = System.Drawing.Color.Black;
+            this.labSunset.Location = new System.Drawing.Point(301, 310);
+            this.labSunset.Name = "labSunset";
+            this.labSunset.Size = new System.Drawing.Size(42, 19);
+            this.labSunset.TabIndex = 8;
+            this.labSunset.Text = "N/A";
+            this.labSunset.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(198, 310);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 19);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Sunset:";
+            // 
+            // labPressure
+            // 
+            this.labPressure.AutoSize = true;
+            this.labPressure.BackColor = System.Drawing.Color.Transparent;
+            this.labPressure.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labPressure.ForeColor = System.Drawing.Color.Black;
+            this.labPressure.Location = new System.Drawing.Point(575, 310);
+            this.labPressure.Name = "labPressure";
+            this.labPressure.Size = new System.Drawing.Size(42, 19);
+            this.labPressure.TabIndex = 12;
+            this.labPressure.Text = "N/A";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(439, 310);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 19);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Pressure:";
+            // 
+            // labWind
+            // 
+            this.labWind.AutoSize = true;
+            this.labWind.BackColor = System.Drawing.Color.Transparent;
+            this.labWind.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labWind.ForeColor = System.Drawing.Color.Black;
+            this.labWind.Location = new System.Drawing.Point(575, 259);
+            this.labWind.Name = "labWind";
+            this.labWind.Size = new System.Drawing.Size(42, 19);
+            this.labWind.TabIndex = 10;
+            this.labWind.Text = "N/A";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Black;
+            this.label11.Location = new System.Drawing.Point(439, 259);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(130, 19);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Wind Speed:";
+            // 
+            // picIcon
+            // 
+            this.picIcon.BackColor = System.Drawing.Color.Transparent;
+            this.picIcon.Location = new System.Drawing.Point(202, 118);
+            this.picIcon.Name = "picIcon";
+            this.picIcon.Size = new System.Drawing.Size(137, 94);
+            this.picIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picIcon.TabIndex = 13;
+            this.picIcon.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(465, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 19);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Temperature: ";
+            // 
+            // labtemp
+            // 
+            this.labtemp.AutoSize = true;
+            this.labtemp.BackColor = System.Drawing.Color.Transparent;
+            this.labtemp.Font = new System.Drawing.Font("Lucida Console", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labtemp.ForeColor = System.Drawing.Color.Black;
+            this.labtemp.Location = new System.Drawing.Point(483, 202);
+            this.labtemp.Name = "labtemp";
+            this.labtemp.Size = new System.Drawing.Size(64, 29);
+            this.labtemp.TabIndex = 15;
+            this.labtemp.Text = "N/A";
+            // 
+            // humidlbl
+            // 
+            this.humidlbl.AutoSize = true;
+            this.humidlbl.BackColor = System.Drawing.Color.Transparent;
+            this.humidlbl.Font = new System.Drawing.Font("Lucida Console", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.humidlbl.ForeColor = System.Drawing.Color.Black;
+            this.humidlbl.Location = new System.Drawing.Point(641, 202);
+            this.humidlbl.Name = "humidlbl";
+            this.humidlbl.Size = new System.Drawing.Size(64, 29);
+            this.humidlbl.TabIndex = 17;
+            this.humidlbl.Text = "N/A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(623, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 19);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Humidity: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.humidlbl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labtemp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picIcon);
+            this.Controls.Add(this.labPressure);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.labWind);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.labSunset);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.labSunrise);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labDetails);
+            this.Controls.Add(this.labCondition);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.citytxt);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Name = "Form1";
+            this.Text = "Ebuka\'s Weather App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox citytxt;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Label labCondition;
+        private System.Windows.Forms.Label labDetails;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labSunrise;
+        private System.Windows.Forms.Label labSunset;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labPressure;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labWind;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox picIcon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labtemp;
+        private System.Windows.Forms.Label humidlbl;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Weather App/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.Globalization;
+using System.Net.Http;
+
+
+
+namespace Weather_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            getWeather();
+        }
+
+        async void getWeather()
+        {
+            string city = citytxt.Text.Trim();
+            string APIKey = "effcb62812d63ea0abbfed8c1c210f51";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+               try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        MessageBox.Show($"{city} not found. Please check the name and try again.", "404 Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    response.EnsureSuccessStatusCode();
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+
+                    picIcon.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                    labCondition.Text = Info.weather[0].main;
+                    labDetails.Text = Info.weather[0].description.ToUpper();
+                    labSunset.Text = ConvertDateTime(Info.sys.sunset).ToShortTimeString();
+                    labSunrise.Text = ConvertDateTime(Info.sys.sunrise).ToShortTimeString();
+                    labWind.Text = Info.wind.speed.ToString();
+                    labPressure.Text = Info.main.pressure.ToString() + "hpa";
+                  
+                    humidlbl.Text = Info.main.humidity.ToString() +"%";
+
+                    double kelvinTemp = Info.main.temp;
+                    double celsiusTemp = kelvinTemp - 273.15;
+                    labtemp.Text = $"{celsiusTemp:F2}°C";
+                }
+                catch (HttpRequestException ex)
+                {
+                    MessageBox.Show($"Network error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        public DateTime ConvertDateTime(long secs)
+        {
+            DateTime day= new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc).ToLocalTime();
+            day=day.AddSeconds(secs).ToLocalTime();
+            return day;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    } }
